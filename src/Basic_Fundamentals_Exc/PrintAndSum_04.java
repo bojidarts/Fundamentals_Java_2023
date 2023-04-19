da0168db85cb431fd09042e22225f25ed76b44ba
bojidarts <tsachev2@gmail.com>
2023-04-19 16:02:48 +0300

    test commit

---
+++ src/Basic_Fundamentals_Exc/PrintAndSum_04.java
@@ -0,0 +1,18 @@
+package Basic_Lab.Basic_Fundamentals_Exc;
+
+import java.util.Scanner;
+
+public class PrintAndSum_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int startNum = Integer.parseInt(scanner.nextLine());
+        int endNum = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+        for (int i = startNum; i <= endNum; i++) {
+            System.out.printf("%d ", i);
+            sum += i;
+        }
+        System.out.printf("%nSum: %d", sum);
+    }
+}
