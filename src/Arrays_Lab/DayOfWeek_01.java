a89356c25b070ba74bb6c52d9f5a739930026048
bojidarts <tsachev2@gmail.com>
2023-10-16 18:18:32 +0300

    test commit

---
+++ src/Arrays_Lab/DayOfWeek_01.java
@@ -0,0 +1,18 @@
+package Basic_Lab.Arrays_Lab;
+
+import java.util.Scanner;
+
+public class DayOfWeek_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String [] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        int number = Integer.parseInt(scanner.nextLine());
+        if (number >= 1 && number <= 7){
+            System.out.println(days [number - 1]);
+        }else {
+            System.out.println("Invalid day!");
+        }
+
+    }
+}
